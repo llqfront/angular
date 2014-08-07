3c7927da75fdca9cda1f0cb75c15ed05f1669986
lester <llqfront@aliyun.com>
2014-08-07 16:18:41 +0800

    fixed

--- angular/js/service.js
+++ angular/js/service.js
@@ -1,6 +1,6 @@
 define(function(require, exports, module){
 	var app = angular.module('factory',[]);
-		.factory('testFactory', function () {
+		app.factory('testFactory', function () {
 		     return {
 		        lable: function(){
 		        	return  [
@@ -11,6 +11,17 @@ define(function(require, exports, module){
 					];
 		        }
 		    }
+		 //   var data = [
+			// 	{'id' : 1, 'name':'Ted', 'total': 5.996},
+			// 	{'id' : 2, 'name':'Michelle', 'total': 10.996},
+			// 	{'id' : 3, 'name':'Zend', 'total': 10.99},
+			// 	{'id' : 4, 'name':'Tina', 'total': 15.996}
+			// ];
+		 //   var factory = {};
+		 //   factory.getData = function(){
+		 //   	 return data;
+		 //   }
+		 //   return factory;
 		});
 	
 })
