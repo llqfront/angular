e5dcd7dc78d1752fe31f2b7e6574f14bfe7ac297
lester <llqfront@aliyun.com>
2014-08-07 16:24:11 +0800

    fixed

--- angular/js/service.js
+++ angular/js/service.js
@@ -1,27 +1,27 @@
 define(function(require, exports, module){
 	var app = angular.module('factory',[]);
 		app.factory('testFactory', function () {
-		     return {
-		        lable: function(){
-		        	return  [
-						{'id' : 1, 'name':'Ted', 'total': 5.996},
-						{'id' : 2, 'name':'Michelle', 'total': 10.996},
-						{'id' : 3, 'name':'Zend', 'total': 10.99},
-						{'id' : 4, 'name':'Tina', 'total': 15.996}
-					];
-		        }
-		    }
-		 //   var data = [
-			// 	{'id' : 1, 'name':'Ted', 'total': 5.996},
-			// 	{'id' : 2, 'name':'Michelle', 'total': 10.996},
-			// 	{'id' : 3, 'name':'Zend', 'total': 10.99},
-			// 	{'id' : 4, 'name':'Tina', 'total': 15.996}
-			// ];
-		 //   var factory = {};
-		 //   factory.getData = function(){
-		 //   	 return data;
-		 //   }
-		 //   return factory;
+		   //   return {
+		   //      lable: function(){
+		   //      	return  [
+					// 	{'id' : 1, 'name':'Ted', 'total': 5.996},
+					// 	{'id' : 2, 'name':'Michelle', 'total': 10.996},
+					// 	{'id' : 3, 'name':'Zend', 'total': 10.99},
+					// 	{'id' : 4, 'name':'Tina', 'total': 15.996}
+					// ];
+		   //      }
+		   //  }
+		   var data = [
+				{'id' : 1, 'name':'Ted', 'total': 5.996},
+				{'id' : 2, 'name':'Michelle', 'total': 10.996},
+				{'id' : 3, 'name':'Zend', 'total': 10.99},
+				{'id' : 4, 'name':'Tina', 'total': 15.996}
+			];
+		   var factory = {};
+		   factory.lable = function(){
+		   	 return data;
+		   }
+		   return factory;
 		});
 	
 })
